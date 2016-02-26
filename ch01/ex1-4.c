dc915a1da5bef015ac0120645f1927243f7e7246
daniel <daniel@debian-daniel>
2016-02-25 21:36:17 -0300

    ex 1-4 finished

---
+++ ch01/ex1-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+/* printf Celsius to Fahrenheit table
+	for fahr = 0, 20, .., 300; floating-point version */
+
+int main () {
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 150;
+	step = 10;
+
+	celsius = lower;
+	printf("Celsius\tFahrenheit\n\n");
+	while (celsius <= upper) {
+		fahr = ((9.0/5.0) * celsius) + 32.0;
+		printf("%3.0f\t\t%6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+	return 0;
+}
