d37b2a6c7b47dd358fdf832b0cd7d02b6bb7ca65
daniel <daniel@debian-daniel>
2016-03-01 21:52:41 -0300

    exercise 12

---
+++ ch01/ex1-12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* print inputs one words per line */
+
+#define	IN 		1
+#define	OUT 	0
+
+int main () {
+
+	int c, state; 
+	state = OUT;
+
+	while((c=getchar()) != EOF) { 
+		if(c == ' ' | c == '\n' | c== '\t') {
+			state = OUT;
+			printf("\n");
+		} else if (state == OUT) {
+				state == IN;
+				putchar(c);
+		  }
+	}
+	return 0;
+}
