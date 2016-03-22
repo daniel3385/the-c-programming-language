8e2934ea9fd701be2e0e2b8c43e1a871b306681d
daniel <daniel@debian-daniel>
2016-03-21 21:44:50 -0300

    ex 03 done

--- ch03/ex03-1.c
+++ ch03/ex03-1.c
@@ -6,3 +6,22 @@ int binsearch(int x, int v[], int n) {
 	int low, high, mid;
 	
 	low = 0;
+	high = n - 1;
+	while(low <= high) {
+		mid = (low + high)/2;
+		if(x < v[mid])
+			high = mid - 1;
+		else if(x > v[mid])
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
+
+void main() {
+
+	int v[] = {1,3,5,7,9,11,13,15};
+	int x = 11;
+	printf("Result:%d ", binsearch(x, v, (sizeof(v)/sizeof(v[0]))));
+}
